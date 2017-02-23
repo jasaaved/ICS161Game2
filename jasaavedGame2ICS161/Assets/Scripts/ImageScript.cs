96b7bc975bec6af3ca86e4ffce172bb98e57e869
jasaaved <jasaaved@uci.edu>
2017-02-23 02:15:44 -0800

    Added missing files

---
+++ jasaavedGame2ICS161/Assets/Scripts/ImageScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageScript : MonoBehaviour {
+    public GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+        RectTransform crt = canvas.GetComponent<RectTransform>();
+        this.GetComponent<RectTransform>().sizeDelta = new Vector2(crt.sizeDelta.x, crt.sizeDelta.y);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ jasaavedGame2ICS161/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject start;
+    public GameObject controls;
+
+    public void Control()
+    {
+        controls.SetActive(true);
+        start.SetActive(false);
+    }
+
+    public void startmenu()
+    {
+        start.SetActive(true);
+        controls.SetActive(false);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("DemoScene");
+    }
+}
